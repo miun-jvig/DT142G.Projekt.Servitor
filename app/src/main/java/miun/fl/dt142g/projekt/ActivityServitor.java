acc1a21a6daf914c172b81efec93ccc5295935a1
miun-jvig <jovi1802@student.miun.se>
2022-11-14 14:56:44 +0100

    Added class "SwitchActivity" for switching between views.

--- app/src/main/java/miun/fl/dt142g/projekt/ActivityServitor.java
+++ app/src/main/java/miun/fl/dt142g/projekt/ActivityServitor.java
@@ -10,20 +10,12 @@ import android.widget.Button;
 public class ActivityServitor extends AppCompatActivity {
     private Button back_button;
 
-    private void NextActivity() {
-        Intent main_activity = new Intent(this, MainActivity.class);
-        startActivity(main_activity);
-    }
-    public class ButtonListener implements View.OnClickListener {
-        @Override
-        public void onClick(View view){ NextActivity(); }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sevitor);
+        Intent activity_main = new Intent(this, MainActivity.class);
         back_button = findViewById(R.id.back_button);
-        back_button.setOnClickListener(new ButtonListener());
+        back_button.setOnClickListener(new SwitchActivity(activity_main));
     }
 }
--- app/src/main/java/miun/fl/dt142g/projekt/MainActivity.java
+++ app/src/main/java/miun/fl/dt142g/projekt/MainActivity.java
@@ -11,15 +11,6 @@ public class MainActivity extends AppCompatActivity {
     private Button button_servitor;
     private Button button_kok;
 
-    private void NextActivity() {
-        Intent activity_servitor = new Intent(this, ActivityServitor.class);
-        startActivity(activity_servitor);
-    }
-    public class ButtonListener implements View.OnClickListener {
-        @Override
-        public void onClick(View view){ NextActivity(); }
-    }
-
     /**
      * on startup
      * @param savedInstanceState - temp
@@ -28,8 +19,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent activity_servitor = new Intent(this, ActivityServitor.class);
         button_servitor = findViewById(R.id.button_servitor);
         button_kok = findViewById(R.id.button_kok);
-        button_servitor.setOnClickListener(new ButtonListener());
+        button_servitor.setOnClickListener(new SwitchActivity(activity_servitor));
     }
 }
