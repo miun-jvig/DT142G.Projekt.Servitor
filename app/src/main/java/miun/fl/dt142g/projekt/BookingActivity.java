71787a5616d5f57bc86fb3299e3f2d0376ec1149
Nikki2001-boop <Nikki.Wolgers@gmail.com>
2022-11-14 16:31:32 +0100

    Added BookingActivity

---
+++ app/src/main/java/miun/fl/dt142g/projekt/BookingActivity.java
@@ -0,0 +1,15 @@
+package miun.fl.dt142g.projekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booking);
+
+    }
+}
