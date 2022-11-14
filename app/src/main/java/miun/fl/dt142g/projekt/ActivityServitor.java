db3db33bca381cf67c8165a5b2b4be0afb85b6ab
miun-jvig <jovi1802@student.miun.se>
2022-11-14 13:46:12 +0100

    Fixed "ActivityServitor"

--- app/src/main/java/miun/fl/dt142g/projekt/ActivityServitor.java
+++ app/src/main/java/miun/fl/dt142g/projekt/ActivityServitor.java
@@ -2,6 +2,13 @@ package miun.fl.dt142g.projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class ActivityServitor extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sevitor);
+    }
 }
