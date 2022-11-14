44d9ec8ddba0f07c15ade149260e0cb3d1189816
Lalezish <alex.licarius@gmail.com>
2022-11-14 14:55:27 +0100

    Added backbutton in serverActivity

--- app/src/main/java/miun/fl/dt142g/projekt/ActivityServitor.java
+++ app/src/main/java/miun/fl/dt142g/projekt/ActivityServitor.java
@@ -2,13 +2,28 @@ package miun.fl.dt142g.projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityServitor extends AppCompatActivity {
+    private Button back_button;
+
+    private void NextActivity() {
+        Intent main_activity = new Intent(this, MainActivity.class);
+        startActivity(main_activity);
+    }
+    public class ButtonListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view){ NextActivity(); }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sevitor);
+        back_button = findViewById(R.id.back_button);
+        back_button.setOnClickListener(new ButtonListener());
     }
 }
