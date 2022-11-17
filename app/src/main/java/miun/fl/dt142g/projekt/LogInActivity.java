79676b0515b1e938ef567b77d705ccbf76984d64
miun-jvig <jovi1802@student.miun.se>
2022-11-17 11:35:10 +0100

    Added functionality to go to "activity_tables" after pressing log-in button.

--- app/src/main/java/miun/fl/dt142g/projekt/LogInActivity.java
+++ app/src/main/java/miun/fl/dt142g/projekt/LogInActivity.java
@@ -2,13 +2,22 @@ package miun.fl.dt142g.projekt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
+    private Button button_log_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        /**
+         * CODE FOR CREDENTIALS - CHECK SQL, if OK, go to intent activity_tables
+         */
+        Intent activity_tables = new Intent(this, TablesActivity.class);
+        button_log_in = findViewById(R.id.button_log_in);
+        button_log_in.setOnClickListener(new SwitchActivity(activity_tables));
     }
 }
