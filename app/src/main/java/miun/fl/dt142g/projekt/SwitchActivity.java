28be09fb507219a0edf2f3b50cc8a500d398a98f
miun-jvig <jovi1802@student.miun.se>
2022-11-17 10:23:39 +0100

    Modified class "LogInActivity". Added "edit_Text_background.xml" to modify editText-elements. Also added documentation on how to use "SwitchActivity".

--- app/src/main/java/miun/fl/dt142g/projekt/SwitchActivity.java
+++ app/src/main/java/miun/fl/dt142g/projekt/SwitchActivity.java
@@ -3,6 +3,16 @@ package miun.fl.dt142g.projekt;
 import android.content.Intent;
 import android.view.View;
 
+/**
+ * A class for switching between activities.
+ *
+ * Typical usage.
+ * <pre>
+ *     Private Button button_new = findViewById(R.id.button_id);
+ *     Intent new_intent = new Intent(this, otherActivity.class);
+ *     button_new.setOnClickListener(new SwitchActivity(new_intent));
+ * </pre>
+ */
 public class SwitchActivity implements View.OnClickListener {
     Intent new_activity;
 
