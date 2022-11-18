c4c446a5ffe3e9a24085cba00d31342fee45a218
miun-jvig <jovi1802@student.miun.se>
2022-11-18 11:30:09 +0100

    Added dropdown-menu in "OrderActivity" class.

--- app/src/main/java/miun/fl/dt142g/projekt/OrderActivity.java
+++ app/src/main/java/miun/fl/dt142g/projekt/OrderActivity.java
@@ -4,12 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.ToggleButton;
+import android.widget.Spinner;
 
 public class OrderActivity extends AppCompatActivity {
     private Button button_back;
+    private Spinner spinner_order;
     // GET FROM SQL
 
 
@@ -18,6 +19,10 @@ public class OrderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
         button_back = findViewById(R.id.button_back_order);
+        spinner_order = findViewById(R.id.order_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.ratter, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner_order.setAdapter(adapter);
 
         Intent activity_booking = new Intent(this, MainActivity.class);
         button_back.setOnClickListener(new SwitchActivity(activity_booking));
