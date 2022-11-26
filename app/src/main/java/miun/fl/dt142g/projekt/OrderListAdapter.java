c1bcf82a794bf5a9ad110ec97970d700934e87e9
Lalezish <alex.licarius@gmail.com>
2022-11-26 17:13:58 +0100

    Added deletebutton in sendorderview.

--- app/src/main/java/miun/fl/dt142g/projekt/OrderListAdapter.java
+++ app/src/main/java/miun/fl/dt142g/projekt/OrderListAdapter.java
@@ -1,16 +1,19 @@
 package miun.fl.dt142g.projekt;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class OrderListAdapter extends ArrayAdapter<Item> {
+public class OrderListAdapter extends ArrayAdapter<Item>{
 
     private final Context context;
     private final ArrayList<Item> order;
@@ -23,12 +26,18 @@ public class OrderListAdapter extends ArrayAdapter<Item> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View rowView = inflater.inflate(R.layout.activity_order_list_view, parent, false);
         TextView textView1 = (TextView) rowView.findViewById(R.id.dish_name);
         TextView textView2 = (TextView) rowView.findViewById(R.id.dish_price);
         TextView textView3 = (TextView) rowView.findViewById(R.id.dish_note);
+        Button deleteButton = (Button) rowView.findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                order.remove(position);
+            }
+        });
         textView1.setText(order.get(position).getName());
         textView2.setText(Double.toString(order.get(position).getPrice())+"kr");
         textView3.setText("- "+order.get(position).getNote());
