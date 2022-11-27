339f33fc96ef2b7fe6ec5a8d02d6af3fab670768
miun-jvig <jovi1802@student.miun.se>
2022-11-27 13:47:22 +0100

    Log-in page now works - only thing missing is adding DB connection.
    If you have previously logged in, then you will stay logged in.

--- app/src/main/java/miun/fl/dt142g/projekt/LogInActivity.java
+++ app/src/main/java/miun/fl/dt142g/projekt/LogInActivity.java
@@ -16,11 +16,22 @@ public class LogInActivity extends AppCompatActivity {
     private Button button_log_in;
     private TextView log_in_error_msg;
     private EditText user_input_pn; // USER INPUT PERSONAL NUMBER
+    public static final String PREFS_NAME = "log_in";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        // SharedPreferences stores value with key PREFS_NAME, works even though app closes
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        boolean has_logged_in = settings.getBoolean(PREFS_NAME, false); // default value false
+        Intent activity_main = new Intent(this, MainActivity.class);
+
+        // If user has previously logged in, then continue
+        if(has_logged_in){
+            startActivity(activity_main);
+        }
+
         // TEMP, IMAGINE PERSONAL NUMBER IS FROM SQL
         ArrayList<String> all_employees = new ArrayList<>();
         String temp_personal_joel = "199408175397";
@@ -28,31 +39,38 @@ public class LogInActivity extends AppCompatActivity {
         all_employees.add(temp_personal_alex);
         all_employees.add(temp_personal_joel);
 
-        // -----
+        // VIEWS
         button_log_in = findViewById(R.id.button_log_in);
         log_in_error_msg = findViewById(R.id.log_in_error);
         user_input_pn = findViewById(R.id.edit_personal_number);
-        button_log_in.setOnClickListener(v -> log_in(all_employees));
+        button_log_in.setOnClickListener(v -> log_in(all_employees, settings));
     }
 
-    public void log_in(ArrayList<String> all_employees){
-        SharedPreferences pref = getSharedPreferences("Activity", Context.MODE_PRIVATE);
-        SharedPreferences.Editor edt = pref.edit();
-        edt.putBoolean("log_in_success", false);
-        edt.apply();
+    /**
+     *  Function to log in only once using SharedPreferences.
+     * @param - List of all personal numbers taken from a database
+     * @param - A SharedPreference containing a value, whether or not user has logged in previously
+     */
+    public void log_in(ArrayList<String> all_employees, SharedPreferences settings){
+        settings = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean(PREFS_NAME, false); // default value false
+        editor.apply();
         String user_input = user_input_pn.getText().toString();
         String error_msg = "Personnumret \"" + user_input + "\" finns ej. Kontakta Anders!";
         Intent activity_main = new Intent(this, MainActivity.class);
         // CHECK IF PERSONAL NUMBER EXISTS IN DB
         for(String pn : all_employees){
             if(user_input.equals(pn)){
-                edt.putBoolean("log_in_success", true);
-                edt.apply();
+                editor.putBoolean(PREFS_NAME, true);
+                editor.apply();
                 startActivity(activity_main);
+                finish();
             }
         }
 
-        if(pref.getBoolean("log_in_success", true)){
+        // CHANGES TEXT DEPENDING IF PERSONAL NUMBER DOES NOT EXIST
+        if(settings.getBoolean(PREFS_NAME, true)){
             log_in_error_msg.setText("");
         }
         else{
